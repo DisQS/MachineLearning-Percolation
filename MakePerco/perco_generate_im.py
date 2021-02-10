e1ea2c70b6e5fab6e3b1b02ba37b08c4adc83d69
Djenabou <55987929+Djenabou@users.noreply.github.com>
2021-02-10 11:28:54 +0100

    py code to generate images

--- MakePerco/perco_generate_im.py
+++ MakePerco/perco_generate_im.py
@@ -2,18 +2,24 @@ import matplotlib.pyplot as plt
 import os
 import re
 import pickle
+import sys 
+import numpy as np
 
 def plot_im_lattice(filename_pkl):
     
     data=pickle.load(open(filename_pkl,"rb"))
     cluster_pbc_norm=data['cluster_pbc_norm']
     filename, file_extension = os.path.splitext(filename_pkl)
-    fig=plt.figure()
-    plt.axis('off')
-    plt.imshow(cluster_pbc_norm,cmap='Greys')
-    plt.imsave(filename+'_im_lattice.png', cluster_pbc_norm,cmap='Greys')
-    plt.close('all')      
-    return
+    if filename+'_im_lattice.png' in os.listdir('.'):
+        return
+
+    else:
+        fig=plt.figure()
+        plt.axis('off')
+        plt.imshow(cluster_pbc_norm,cmap='Greys')
+        plt.imsave(filename+'_im_lattice.png', cluster_pbc_norm,cmap='Greys')
+        plt.close('all')      
+        return
     
     
     
@@ -25,39 +31,42 @@ def plot_corr_funcs(filename_corr, filename_pkl):
     proba_largest=data['proba largest']
 
     filename, file_extension = os.path.splitext(filename_corr)
-    L_size=filename.split('_')[8]      
-    regex1 = re.compile('\d+')
-    size_sys_reg=re.findall(regex1,L_size)
-    size_sys=int(size_sys_reg[0])
+    if filename+'_im_lattice.png' in os.listdir('.'):
+        return
+    else:
+        L_size=filename.split('_')[8]      
+        regex1 = re.compile('\d+')
+        size_sys_reg=re.findall(regex1,L_size)
+        size_sys=int(size_sys_reg[0])
     
-    p_occ=filename.split('_')[7]      
-    regex2 = re.compile('\d+\.\d+')
-    p_reg=re.findall(regex2,p_occ)
-    p=float(p_reg[0])
+        p_occ=filename.split('_')[7]      
+        regex2 = re.compile('\d+\.\d+')
+        p_reg=re.findall(regex2,p_occ)
+        p=float(p_reg[0])
     
     
    
-    plt.plot(corr_data[0],corr_data[1],label='g(r) every clusters')
-    plt.plot(corr_data[3],corr_data[4], label='largest cluster')
-    plt.axhline(y=square_proba, color='g', linestyle='--')
-    plt.axhline(y=proba_largest, color='grey', linestyle='--')
-    plt.legend(loc='best')
-    plt.xlabel('distance r')
-    plt.ylabel('correlation function g(r)')
-    plt.title('Correlation function for system '+str(size_sys)+' at density '+str(p))
-    plt.savefig(filename+'.png')
-    plt.close('all')
+        plt.plot(corr_data[0],corr_data[1],label='g(r) every clusters')
+        plt.plot(corr_data[3],corr_data[4], label='largest cluster')
+        plt.axhline(y=square_proba, color='g', linestyle='--')
+        plt.axhline(y=proba_largest, color='grey', linestyle='--')
+        plt.legend(loc='best')
+        plt.xlabel('distance r')
+        plt.ylabel('correlation function g(r)')
+        plt.title('Correlation function for system '+str(size_sys)+' at density '+str(p))
+        plt.savefig(filename+'.png')
+        plt.close('all')
     
-    plt.plot(corr_data[0],corr_data[1],label='g(r) every clusters')
-    plt.plot(corr_data[3],corr_data[4], label='largest cluster')
-    plt.legend(loc='best')
-    plt.yscale('log')
-    plt.xlabel('distance r')
-    plt.ylabel('correlation function g(r)')
-    plt.title('Correlation function (log scale) for system '+str(size_sys)+' at density '+str(p))
-    plt.savefig(filename+'log'+'.png')
-    plt.close('all')
-    return
+        plt.plot(corr_data[0],corr_data[1],label='g(r) every clusters')
+        plt.plot(corr_data[3],corr_data[4], label='largest cluster')
+        plt.legend(loc='best')
+        plt.yscale('log')
+        plt.xlabel('distance r')
+        plt.ylabel('correlation function g(r)')
+        plt.title('Correlation function (log scale) for system '+str(size_sys)+' at density '+str(p))
+        plt.savefig(filename+'log'+'.png')
+        plt.close('all')
+        return
 #############################################################################################
 filename_pkl= str(sys.argv[1])
 filename_corr= str(sys.argv[2])
