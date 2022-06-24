736d3fe7a612990ceeff9b4145c24b1696e0fcc6
Djena Bayo <phrhmb@mistral.theory.warwick.ac.uk>
2022-06-24 15:10:22 +0100

    bash script for RandomCrosses

---
+++ MakePerco/perco_RandomCrosses.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+dir=${1:-../data}   
+
+#seed=${2:-1234567}
+size=${2:-10}
+perco_i=${3:-05927}
+perco_f=${4:-06000}
+dperco=${5:-02000}
+configs=${6:-2}
+py=${7:RandomCrosses.py}
+
+codedir=`pwd`
+
+echo "PERCO: dir=" $dir ", size=" $size \
+", [Ti,Tf,dT]= [" $perco_i, $perco_f, $dperco "], configs=" $configs",py_code="$py
+
+mkdir -p $dir
+cd $dir
+mkdir -p "L"$size"_cross"
+cd "L"$size"_cross"
+
+for perco in $(seq $perco_i $dperco $perco_f) 
+do
+
+echo "--- making jobfile for p=" $perco
+
+jobfile=`printf "$perco.sh"`
+echo $jobfile
+
+cat > ${jobfile} << EOD
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2012
+#SBATCH --time=48:00:00
+
+module load Anaconda3
+#conda init --all; conda activate
+
+pwd
+echo "--- working on p=$perco"
+
+python $codedir/perco_generate_data_with_cross.py $size $perco $perco 1 $configs
+
+chmod -R g+w *
+
+echo "--- finished with p=$perco"
+EOD
+
+cat ${jobfile}
+
+chmod 755 ${jobfile}
+chmod g+w ${jobfile}
+#(sbatch -q devel ${jobfile})
+(sbatch -q taskfarm ${jobfile})
+#(sbatch ${jobfile})
+#(./${jobfile})
+
+done
