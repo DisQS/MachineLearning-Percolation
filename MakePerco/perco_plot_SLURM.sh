11a58e5f5ee27b03db8bbdbbf03ef5474e0d07e9
Djena Bayo <phrhmb@mistral.theory.warwick.ac.uk>
2024-03-12 01:51:44 +0000

    Fixed issue with module and ext input

--- MakePerco/perco_plot_SLURM.sh
+++ MakePerco/perco_plot_SLURM.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 dir=${1:-../data}
-#size=${2:-10}
-option=${2:-0}
-cores=${3:-1}
-py=${4:-perco_generate_plot.py}
+size=${2:-10}
+option=${3:-0}
+cores=${4:-1}
+ext=${5:-.png}
+
   
 codedir=`pwd`
 
-echo "PERCO: dir=" $dir ", size=" $size ", option=" $option ", cores=" $cores",py=" $py
+echo "PERCO: dir=" $dir ", size=" $size ", option=" $option ", cores=" $cores ", ext=" $ext
 
 cd $dir
-#cd "L"$size
+cd "L"$size
 
 for directory in p0*
 do
@@ -29,19 +30,23 @@ cat > ${jobfile} << EOD
 #SBATCH --mem-per-cpu=2012
 #SBATCH --time=48:00:00
 
-module load Anaconda3/2019.03
-module load GCCcore/8.3.0
-module load parallel/20190922
+
+module restore Python_3_9_6
+#module load GCC/11.2.0
+#module load OpenMPI/4.1.1 matplotlib/3.4.3
+
+module load parallel/20210722
 
 #conda init --all; conda activate
 
 pwd
 echo "--- working in directory=$directory"
 
-#python $codedir/perco_generate_plot.py $option $pklfile `basename $pklfile .pkl`.cor
-ls *.pkl| parallel -j$cores -a - python $codedir/$py 0 {} {}
 
-chmod -R g+w *.pdf
+#python $codedir/perco_generate_plot.py $option $pklfile `basename $pklfile .pkl`.cor $ext
+ls *.pkl| parallel -j$cores -a - python $codedir/perco_generate_plot.py $option {} `basename {} .pkl`.cor $ext
+
+chmod -R g+w *$ext
 
 #echo "--- finished in directory=  $directory"
 EOD
